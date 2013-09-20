f8075c3fb27e1f278843ad50480e1c5610e99270
gianlucagenga <gianluca.genga@mondora.com>
2013-09-20 18:06:57 +0200

    login automatica dopo invito - primo commit

--- client/invitationToken.js
+++ client/invitationToken.js
@@ -2,12 +2,31 @@ Meteor.subscribe('invitationToken');
 
 function selectedToken(){
    // return InvitationToken.find(); // tutti i token
-    var token =  InvitationToken.findOne( {token: this.location.href.substring(this.location.href.indexOf('?')+1)}); // solo il token corrente
+    var tokenString = this.location.href.substring(this.location.href.indexOf('?')+1);
+    var token =  InvitationToken.findOne( {token: tokenString}); // solo il token corrente
     if (token) {
         // se il token viene riconosciuto,
         // creazione utente
+
         // login utente
+        // todo commentato perchè non testato
+    /*    Meteor.loginWithGoogle({
+            requestPermissions: ['email', 'profile']
+        }, function (err) {
+            if(err) {
+                //error handling
+                alert('error : '+ err);
+                throw new Meteor.Error(Accounts.LoginCancelledError.numericError, 'Error');
+            } else {
+                //show an alert
+                // alert('logged in');
+            }
+        });
+    */
         // rimozione token
+        // todo da trovare altra soluzione.. con la remove viene fatto il refresh della pagina e si atterra sempre su template noMatchToken
+        //InvitationToken.remove(token._id);
+
     }
     return token;
 }
