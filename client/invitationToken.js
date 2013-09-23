3095170933e849bac23d679ff3ba1ce2a4e0df7c
gianlucagenga <gianluca.genga@mondora.com>
2013-09-23 18:14:28 +0200

    subscription confirm + dynamic team view

--- client/invitationToken.js
+++ client/invitationToken.js
@@ -1,38 +1,87 @@
 Meteor.subscribe('invitationToken');
 
-function selectedToken(){
-   // return InvitationToken.find(); // tutti i token
-    var tokenString = this.location.href.substring(this.location.href.indexOf('?')+1);
-    var token =  InvitationToken.findOne( {token: tokenString}); // solo il token corrente
+function selectedTokenAndLogin(){
+
+    var token =  getToken();
+
     if (token) {
-        // se il token viene riconosciuto,
-        // creazione utente
+        // se il token viene riconosciuto
 
         // login utente
-        // todo commentato perchè non testato
-    /*    Meteor.loginWithGoogle({
+        Meteor.loginWithGoogle({
             requestPermissions: ['email', 'profile']
         }, function (err) {
             if(err) {
                 //error handling
-                alert('error : '+ err);
+                alert('error : '+ err + '; '+err.message);
                 throw new Meteor.Error(Accounts.LoginCancelledError.numericError, 'Error');
             } else {
                 //show an alert
                 // alert('logged in');
             }
         });
-    */
-        // rimozione token
-        // todo da trovare altra soluzione.. con la remove viene fatto il refresh della pagina e si atterra sempre su template noMatchToken
-        //InvitationToken.remove(token._id);
+
 
     }
     return token;
 }
 
+function getToken () {
+    // return InvitationToken.find(); // tutti i token
+    var tokenString = this.location.href.substring(this.location.href.indexOf('?')+1);
+    var token =  InvitationToken.findOne( {token: tokenString}); // solo il token corrente
+
+    return token;
+}
+
 Template.joinTeam.helpers({
     tokens: function() {
-        return selectedToken() ;
+        return selectedTokenAndLogin() ;
     }
-})
+});
+
+
+Template.joinTeam.events({
+
+    'click .confirmSubscription' : function () {
+
+        var currentTeam = Session.get('currentTeam');
+        var currentProduct = Session.get('currentProduct');
+
+        try {
+
+            // l'utente viene inserito all'interno di un prodotto
+            var elementProduct = Products.find({slug: currentProduct}).fetch()[0];
+            var productId = elementProduct._id;
+            var arrayAllowedUsers = elementProduct.allowedusers;
+            var index = $.inArray(Meteor.userId(), arrayAllowedUsers);
+
+            if (index == -1) {
+                // se il cliente non è contenuto nel prodotto corrente, lo aggiungo
+                arrayAllowedUsers.push(Meteor.userId());
+                Products.update( {_id: productId} , {$set:{allowedusers: arrayAllowedUsers}} );
+
+            }
+
+            var token = getToken();
+       //     if (token)
+       //         InvitationToken.remove(token._id);
+
+            location.href="/"+currentProduct+"/team/"+currentTeam;
+
+        }
+
+        catch(err) {
+            $.pnotify({
+                title: 'Error during subscription!',
+                text: '\n'+err.message,
+                type: 'error',
+                hide: false
+            });
+        }
+
+        //prevent reload
+        return false;
+    }
+
+});
--- client/products.js
+++ client/products.js
@@ -4,8 +4,37 @@ function selectedProduct(){
     return Products.findOne( {slug: Session.get('currentProduct') } );
 }
 
+function filterUserTeam(){
+
+    var product =  selectedProduct();
+    if (product) {
+        var userData = new Array();
+        var userIds = selectedProduct().allowedusers;
+
+        for (var i=0; i<userIds.length; i++) {
+            var currentUser = Meteor.users.findOne({_id: userIds[i]});
+            if (currentUser){
+
+                var userItem = {
+                    name: currentUser.profile.name,
+                    picture: currentUser.profile.picture
+                };
+
+                userData.push(userItem);
+            }
+        }
+        return userData;
+    }
+}
+
 Template.product.helpers({
     product: function() {
         return selectedProduct() ;
     }
-})
+});
+
+Template.userTeam.helpers ({
+    users: function() {
+        return filterUserTeam() ;
+    }
+});
